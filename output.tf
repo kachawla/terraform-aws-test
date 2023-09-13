a53a483f44b3dc91a598b211b370ae82022cbf23
Karishma Chawla <kachawla@microsoft.com>
2023-09-13 08:54:31 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,3 @@
-output "host" {
-  value = aws_memorydb_cluster.memorydb_cluster.cluster_endpoint[0].address
-}
-
-output "port" {
-  value = aws_memorydb_cluster.memorydb_cluster.cluster_endpoint[0].port
-}
-
-output "connectionString" {
-  value = format("rediss://%s:%d", aws_memorydb_cluster.memorydb_cluster.cluster_endpoint[0].address, aws_memorydb_cluster.memorydb_cluster.cluster_endpoint[0].port)
-  sensitive = true
-}
-
 output "result" {
   value = {
     resources = [aws_memorydb_cluster.memorydb_cluster.id]
