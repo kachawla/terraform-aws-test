4e1e15602da2d0dd1e7816b1148c4aec3c18c633
Karishma Chawla <kachawla@microsoft.com>
2023-09-13 10:36:17 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,5 @@
 output "result" {
   value = {
-    resources = [aws_memorydb_cluster.memorydb_cluster.id]
     values = {
       host = aws_memorydb_cluster.memorydb_cluster.cluster_endpoint[0].address
       port = aws_memorydb_cluster.memorydb_cluster.cluster_endpoint[0].port
