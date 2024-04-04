e1579c1e1248e2847483ed3d70ae0f1d06ec060a
Karishma Chawla <kachawla@microsoft.com>
2024-04-03 17:44:04 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,6 +3,7 @@ output "result" {
     values = {
       host = aws_memorydb_cluster.memorydb_cluster.cluster_endpoint[0].address
       port = aws_memorydb_cluster.memorydb_cluster.cluster_endpoint[0].port
+      tls = true
     }
   }
 }
